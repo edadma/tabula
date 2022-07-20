c9a40aee2e8e4eafc5b98e9b1dfdca6d665adf11
Edward A Maxedon Sr <edadma@gmail.com>
2022-07-20 11:29:57 -0400

    fix typo

--- shared/src/main/scala/com/vinctus/scandas/Type.scala
+++ shared/src/main/scala/com/vinctus/scandas/Type.scala
@@ -14,32 +14,32 @@ object Type:
       case "Bool" | "bool"     => BoolType
 
 abstract class Type(val name: String, val numerical: Boolean):
-  def convert(a: Any, coerse: Boolean = false): Option[Any]
+  def convert(a: Any, coerce: Boolean = false): Option[Any]
 
 case object InferType extends Type("infer", false):
-  def convert(a: Any, coerse: Boolean): Option[Any] = sys.error("InferType.convert()")
+  def convert(a: Any, coerce: Boolean): Option[Any] = sys.error("InferType.convert()")
 
 //case object MixedType extends Type("mixed", false):
-//  def convert(a: Any, coerse: Boolean): Option[Any] = sys.error("MixedType.convert()")
+//  def convert(a: Any, coerce: Boolean): Option[Any] = sys.error("MixedType.convert()")
 
 case object UnknownType extends Type("unknown", false):
-  def convert(a: Any, coerse: Boolean): Option[Any] = sys.error("UnknownType.convert()")
+  def convert(a: Any, coerce: Boolean): Option[Any] = sys.error("UnknownType.convert()")
 
 case object IntType extends Type("int", true):
   private val LongMinDouble = Long.MinValue.toDouble
   private val LongMaxDouble = Long.MaxValue.toDouble
 
-  def convert(a: Any, coerse: Boolean = false): Option[Any] =
+  def convert(a: Any, coerce: Boolean = false): Option[Any] =
     a match
       case null                             => Some(null)
       case n: Int                           => Some(n.toLong)
       case _: Long                          => Some(a)
-      case n: Double if coerse || n.isWhole => if LongMinDouble <= n && n <= LongMaxDouble then Some(n.toLong) else None
-      case s: String => s.toLongOption orElse (if coerse then s.toDoubleOption.map(_.toLong) else None)
+      case n: Double if coerce || n.isWhole => if LongMinDouble <= n && n <= LongMaxDouble then Some(n.toLong) else None
+      case s: String => s.toLongOption orElse (if coerce then s.toDoubleOption.map(_.toLong) else None)
       case _         => None
 
 case object FloatType extends Type("float", true):
-  def convert(a: Any, coerse: Boolean = false): Option[Any] =
+  def convert(a: Any, coerce: Boolean = false): Option[Any] =
     a match
       case null      => Some(null)
       case n: Int    => Some(n.toDouble)
@@ -49,7 +49,7 @@ case object FloatType extends Type("float", true):
       case _         => None
 
 case object BoolType extends Type("bool", false):
-  def convert(a: Any, coerse: Boolean = false): Option[Any] =
+  def convert(a: Any, coerce: Boolean = false): Option[Any] =
     a match
       case null                          => Some(null)
       case b: Boolean                    => Some(b)
@@ -58,10 +58,10 @@ case object BoolType extends Type("bool", false):
       case _                             => None
 
 case object StringType extends Type("string", false):
-  def convert(a: Any, coerse: Boolean = false): Option[Any] = Some(if a == null then null else a.toString)
+  def convert(a: Any, coerce: Boolean = false): Option[Any] = Some(if a == null then null else a.toString)
 
 case object TimestampType extends Type("timestamp", false):
-  def convert(a: Any, coerse: Boolean = false): Option[Any] =
+  def convert(a: Any, coerce: Boolean = false): Option[Any] =
     a match
       case null       => Some(null)
       case t: Instant => Some(t)
